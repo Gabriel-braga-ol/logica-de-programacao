5811778705e6d20e9ba75ac6a23027632ea7c8a2
Gabriel-braga-ol <bielbraga37@outlook.com>
2026-04-22 11:59:06 -0300

    criei o ex for

---
+++ aula 4/for.py
@@ -0,0 +1,15 @@
+# for i in range(1, 10, 2): #também podemos definir um valor inicial
+#     print(i)
+
+# frase = 'Lógica de programaçã e algortimos'
+# for i in range(0, len(frase), 1):
+#     print(frase[i], end='')
+
+# soma = 0
+# qtd = 0
+# for i in range(0, 101, 2):
+#         soma += i
+#         qtd += 1
+# media = soma / qtd
+# print(f'A media dos números pares é: {media}')
+
