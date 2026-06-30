6c8e2d4b5fd6fbc90138a9ac29c5a8f5b0b06287
Gabriel-braga-ol <bielbraga37@outlook.com>
2026-06-29 22:31:19 -0300

    mais leetcode

---
+++ leetcode/ex006.py
@@ -0,0 +1,23 @@
+"""
+Given a sorted array of distinct integers and a target value, 
+return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
+"""
+
+nums = [1,3,5,6]
+
+
+def insert(nums, alvo):
+    for i, j in enumerate(nums):
+        if j >= alvo:
+            return i
+    return len(nums)
+
+print(insert(nums, 2))
+    
+   
+
+            
+
+        
+        
